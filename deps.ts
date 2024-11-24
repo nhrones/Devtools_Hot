aa37d595d1ee134236ecc56db2ff1152834be9e1
NDH <ndhrones@gmail.com>
2024-11-23 19:57:03 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -3,6 +3,6 @@ export { serveFile } from "jsr:@std/http";
 export { debounce } from "jsr:@std/async";
 export * as esBuild from "https://deno.land/x/esbuild@v0.24.0/mod.js";
 export { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
-export * from "../Builder/buildIt.ts";
+export * from "https://raw.githubusercontent.com/nhrones/Devtools_Builder/refs/heads/main/buildIt.ts"  //"../Builder/buildIt.ts";
 export * from "jsr:@ndh/browser";
 export * from "../Config/mod.ts";
