256baa4f2a5ef3907acd7e3d4de2c95b57c345f0
NDH <ndhrones@gmail.com>
2024-11-25 16:03:21 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
 
 /** ============= exports @std  ============= */
-export { join } from "jsr:@std/path";
-export { serveFile } from "jsr:@std/http";
-export { debounce } from "jsr:@std/async";
+export { join } from "jsr:@std/path@1.0.8";
+export { serveFile } from "jsr:@std/http@1.0.11";
+export { debounce } from "jsr:@std/async@1.0.9";
 
 /** ============= exports @ndh  ============= */
-export * from "jsr:@ndh/browser";
-export * from "jsr:@ndh/build"
+export * from "jsr:@ndh/browser@1.0.3";
+export * from "jsr:@ndh/build@1.0.0"
 export type { Config } from "jsr:@ndh/config@1.0.0";
 export { getConfig } from "jsr:@ndh/config@1.0.0";
