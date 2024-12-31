db80c5988ffc886d59df1375ba5414d42e01775d
NDH <nick@ndhrones.com>
2024-12-30 19:18:35 -0500

    upgrade

--- deps.ts
+++ deps.ts
@@ -7,5 +7,5 @@ export { debounce } from "jsr:@std/async@1.0.9";
 /** ============= exports @ndh  ============= */
 export * from "jsr:@ndh/browser@1.0.3";
 export * from "jsr:@ndh/build@1.0.6"
-export type { Config } from "jsr:@ndh/config@1.0.2";
-export { getConfig } from "jsr:@ndh/config@1.0.2";
+export type { Config } from "jsr:@ndh/config@1.0";
+export { getConfig } from "jsr:@ndh/config@1.0";
