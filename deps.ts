c01a37fb0d492ef3052746baf95f761c20dfdf96
NDH <nick@ndhrones.com>
2024-12-31 12:06:48 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -7,5 +7,5 @@ export { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11.0";
 export { build, stop } from "npm:esbuild@0.24.0";
 
 /** ============= exports @ndh  ============= */
-export type { Config } from "jsr:@ndh/config@1.0.3";
-export { getConfig } from "jsr:@ndh/config@1.0.3";
+export type { Config } from "jsr:@ndh/config@1.0.4";
+export { getConfig } from "jsr:@ndh/config@1.0.4";
