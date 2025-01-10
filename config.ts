38d029953fe783a3bf018043f59da0060f213755
NDH <nick@ndhrones.com>
2025-01-10 12:37:27 -0500

    update

--- config.ts
+++ config.ts
@@ -2,24 +2,6 @@ import { Config, getConfig } from './deps.ts'
 import JSR from "./jsr.json" with { type: "json" };
 export const VER = JSR.version
 
-// If the first CLI argument (args[0]) = -h or ?
-// show help text then just exit
-if (Deno.args[0] === '-h' || Deno.args[0] === '?') {
-   console.log(`
-Build Help --   
-Usage:
-Commandline args:
--h or ? = this help
-
-/src/dev.json - build:
-OutPath: string - the folder to place the bundle in (defaults to 'dist')
-Entry: string[] - default = ["./src/main.ts"]
-Watch: string - the folder to watch for change
-Minify: boolean - true or false (defaults to false)`
-   );
-   Deno.exit(0)
-}
-
 // initial default configuration for this app
 const requiredCfg = {
    "BundleName": "bundle.js",    // the name of the esBundle file
--- deps.ts
+++ deps.ts
@@ -7,5 +7,5 @@ export { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11.0";
 export { build, stop } from "npm:esbuild@0.24.0";
 
 /** ============= exports @ndh  ============= */
-export type { Config } from "jsr:@ndh/config@1.1.2";
-export { getConfig } from "jsr:@ndh/config@1.1.2";
+export type { Config } from "jsr:@ndh/config@1.1.4";
+export { getConfig } from "jsr:@ndh/config@1.1.4";
